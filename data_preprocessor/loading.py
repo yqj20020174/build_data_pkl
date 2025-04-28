00398e28879ed8618eabb86f3f3af31138e54c6b
yqj <143869539+yqj20020174@users.noreply.github.com>
2025-04-28 20:42:34 +0800

    Create loading.py

---
+++ data_preprocessor/loading.py
@@ -0,0 +1,63 @@
+@PIPELINES.register_module()
+class LoadMultiViewImageFromFiles:
+    """Load multi channel images from a list of separate channel files.
+
+    Expects results['image_paths'] to be a list of filenames.
+
+    Args:
+        to_float32 (bool): Whether to convert the img to float32.
+            Defaults to False.
+        color_type (str): Color type of the file. Defaults to 'unchanged'.
+    """
+
+    def __init__(self, to_float32=False, color_type="unchanged"):
+        self.to_float32 = to_float32
+        self.color_type = color_type
+
+    def __call__(self, results):
+        """Call function to load multi-view image from files.
+
+        Args:
+            results (dict): Result dict containing multi-view image filenames.
+
+        Returns:
+            dict: The result dict containing the multi-view image data. \
+                Added keys and values are described below.
+
+                - filename (str): Multi-view image filenames.
+                - img (np.ndarray): Multi-view image arrays.
+                - img_shape (tuple[int]): Shape of multi-view image arrays.
+                - ori_shape (tuple[int]): Shape of original image arrays.
+                - pad_shape (tuple[int]): Shape of padded image arrays.
+                - scale_factor (float): Scale factor.
+                - img_norm_cfg (dict): Normalization configuration of images.
+        """
+        filename = results["image_paths"]
+        # img is of shape (h, w, c, num_views)
+        # modified for waymo
+        images = []
+        h, w = 0, 0
+        for name in filename:
+            images.append(Image.open(name))
+        
+        #TODO: consider image padding in waymo
+
+        results["filename"] = filename
+        # unravel to list, see `DefaultFormatBundle` in formating.py
+        # which will transpose each image separately and then stack into array
+        results["img"] = images
+        # [1600, 900]
+        results["img_shape"] = images[0].size
+        results["ori_shape"] = images[0].size
+        # Set initial values for default meta_keys
+        results["pad_shape"] = images[0].size
+        results["scale_factor"] = 1.0
+        
+        return results
+
+    def __repr__(self):
+        """str: Return a string that describes the module."""
+        repr_str = self.__class__.__name__
+        repr_str += f"(to_float32={self.to_float32}, "
+        repr_str += f"color_type='{self.color_type}')"
+        return repr_str
